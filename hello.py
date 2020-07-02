bb06149977cb9a8713ff2d2e7aee8ed4e1eec2e4
sfranco78 <sfranco78@gmail.com>
2020-07-02 16:17:30 -0500

    Programa hola mundo en Python

---
+++ hello.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":   #Punto de inicio el programa. Variables del sistema, indica cómo se llama el modulo o archivo que se esta ejecutando
+
+    print("Hola mundo, Sergio Franco C")
