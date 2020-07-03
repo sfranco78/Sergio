b85648fcc12bac51e01afe5c15e24017b0437545
sfranco78 <sfranco78@gmail.com>
2020-07-03 18:05:28 -0500

    Archivo de pruebas

--- hello.py
+++ hello.py
@@ -1,3 +1,26 @@
 if __name__ == "__main__":   #Punto de inicio el programa. Variables del sistema, indica cómo se llama el modulo o archivo que se esta ejecutando
 
     print("Hola mundo, Sergio Franco C")
+
+#Cadenas de texto
+    nombre = "Sergio"
+
+#Enteros
+    edad = 40
+
+# listas
+    materias = ['Precalculo', 'Estadistica', 'Ingeniería de Software']
+
+def saludo(entrada):
+    print(entrada)
+
+    saludo("Hola nuevamente")
+
+#Esta es una en trada para calcular tu edad#
+
+print("¿Cómo se llama?")
+nombre = input()
+print(f"Me alegro de conocerle, {nombre}")
+
+# Funciones
+
